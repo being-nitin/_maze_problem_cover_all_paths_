6a5dee9a70202dfee71a56ad0a2ca822ba2f6ae1
Nitin <nsinghboss02@gmail.com>
2022-02-06 12:26:38 +0530

    Returned the paths in qa martrix and given with backtracking.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -11,7 +11,9 @@ public class Main {
                 {true,true,true},
                 {true,true,true }
         };
-        allPaths("",maze,0,0);
+//        allPaths("",maze,0,0);
+        int[][] mazze = new int[maze.length][maze[0].length];
+        matrixPath("",maze,0,0,mazze,1);
     }
     public static void allPaths(String p,boolean[][] maze,int r,int c){
         if(r==maze.length-1 && c==maze[0].length-1){
@@ -42,5 +44,41 @@ public class Main {
         // this line where the function will be over.
         // so before the functions get removed, also remove the changes that were made by that function.
         maze[r][c] = true;
+    }
+    // now printing the matrix of path.
+    public static void matrixPath(String p,boolean[][] maze, int r,int c,int[][] path,int steps) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = steps;
+            for(int[] arr:path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if(!maze[r][c]) {
+            return;
+        }
+        maze[r][c] = false;
+        path[r][c] = steps;
+        // for down.
+        if (r < maze.length - 1) {
+            matrixPath(p + 'D', maze, r + 1, c, path, steps + 1);
+        }
+        // for right
+        if (c < maze[0].length - 1) {
+            matrixPath(p + 'R', maze, r, c + 1, path, steps + 1);
+        }
+        // for up
+        if (r > 0) {
+            matrixPath(p + 'U', maze, r - 1, c, path, steps + 1);
+        }
+        // for left
+        if (c > 0) {
+            matrixPath(p + 'L', maze, r, c - 1, path, steps + 1);
+        }
+        // now the condition of backtracking.
+        maze[r][c] = true;
+        path[r][c] = 0;
     }
 }
